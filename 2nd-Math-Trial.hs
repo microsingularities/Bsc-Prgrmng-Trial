63bbd8cc3d542f75ec99fd4d43b46705f149ad5c
Rizki Susilo Utomo <Rizkisusilo@gmail.com>
2015-09-01 10:34:27 +0700

    Create 2nd-Math-Trial.hs

---
+++ 2nd-Math-Trial.hs
@@ -0,0 +1,20 @@
+import Data.List
+
+test1 x = x * x * x
+
+-- y = F(x) = x^2 + x
+-- f(x) => (f x)
+sqr x = x * x
+
+-- sqr 10 = 10 * 10
+
+akar x = x * x * x
+
+cube x = (sqr x) * x
+
+our_pi = 3.145735872794
+
+-- data + function
+-- data => domains
+-- data types, Int, Integer, Char, Float
+-- data type Bool
